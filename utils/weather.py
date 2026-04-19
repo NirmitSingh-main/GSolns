853ab9775691366bc589c70a6698131cd4bbf01a
SagitQuark <manasvishwa88@gmail.com>
2026-04-19 22:59:42 +0530

    fixed bug in weather.py

--- utils/weather.py
+++ utils/weather.py
@@ -1,15 +1,22 @@
-from flask.cli import load_dotenv
+from dotenv import load_dotenv
 import requests
 import os
+
 load_dotenv()
 
-Weather_API_KEY = os.getenv("Weather_API_KEY")
+API_KEY = os.getenv("Weather_API_KEY")
 
 def get_weather(city):
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}"
-    response = requests.get(url).json()
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
     
-    weather = response['weather'][0]['main']
-    temp = response['main']['temp']
+    response = requests.get(url)
+    
+    if response.status_code != 200:
+        return "Unknown", 0
+    
+    data = response.json()
+    
+    weather = data['weather'][0]['main']
+    temp = data['main']['temp']
     
     return weather, temp
