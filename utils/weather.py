21a3f3e311234a90b65ee120c3ff02c231f82bbd
SagitQuark <manasvishwa88@gmail.com>
2026-04-19 23:57:12 +0530

    Awai changes kare the

--- utils/weather.py
+++ utils/weather.py
@@ -3,20 +3,30 @@ import requests
 import os
 
 load_dotenv()
-
 API_KEY = os.getenv("Weather_API_KEY")
 
 def get_weather(city):
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+    url = "https://api.openweathermap.org/data/2.5/weather"
     
-    response = requests.get(url)
+    params = {
+        "q": city,
+        "appid": API_KEY,
+        "units": "metric"
+    }
     
-    if response.status_code != 200:
-        return "Unknown", 0
+    try:
+        response = requests.get(url, params=params)
+        
+        if response.status_code != 200:
+            return "Clear", 25
+        
+        data = response.json()
+        
+        weather = data['weather'][0]['main']
+        temp = data['main']['temp']
+        
+        return weather, temp
     
-    data = response.json()
+    except:
+        return "Clear", 25
     
-    weather = data['weather'][0]['main']
-    temp = data['main']['temp']
-    
-    return weather, temp
